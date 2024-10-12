fefd74f2b1175c723eb2e73e0ad4be5afff93a2a
LuzKindaGud <dinhvansang2006@gmail.com>
2024-10-12 19:59:24 +0700

    OOP Java Chapter 01

---
+++ src/Ex1_08.java
@@ -0,0 +1,82 @@
+class Time{
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Time(int hour, int minute, int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour(){
+        return hour;
+    }
+
+    public int getMinute(){
+        return minute;
+    }
+
+    public int getSecond(){
+        return second;
+    }
+
+    public void setHour(int hour){
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute){
+        this.minute = minute;
+    }
+
+    public void setSecond(int second){
+        this.second = second;
+    }
+
+    public void setTime(int hour, int minute, int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public Time nextSecond(){
+        return new Time(hour, minute, second + 1);
+    }
+
+    public Time previousSecond(){
+        return new Time(hour, minute, second - 1);
+    }
+
+    public String toString(){
+        return hour + ":" + minute + ":" + second;
+    }
+}
+
+public class Ex1_08 {
+    public static void main(String[] args) {
+        // Test constructors and toString()
+        Time t1 = new Time(1, 2, 3);
+        System.out.println(t1);  // toString()
+
+        // Test Setters and Getters
+        t1.setHour(4);
+        t1.setMinute(5);
+        t1.setSecond(6);
+        System.out.println(t1);  // toString()
+        System.out.println("Hour: " + t1.getHour());
+        System.out.println("Minute: " + t1.getMinute());
+        System.out.println("Second: " + t1.getSecond());
+
+        // Test setTime()
+        t1.setTime(23, 59, 58);
+        System.out.println(t1);  // toString()
+
+        // Test nextSecond();
+        System.out.println(t1.nextSecond());
+        System.out.println(t1.nextSecond().nextSecond());
+
+        // Test previousSecond()
+        System.out.println(t1.previousSecond());
+        System.out.println(t1.previousSecond().previousSecond());
+    }
+}
